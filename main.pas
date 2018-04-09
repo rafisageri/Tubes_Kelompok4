2bb0962db696564a22a577a4f385e57f07fe227d
hafizhmh <38131950+hafizhmh@users.noreply.github.com>
2018-04-10 00:28:45 +0700

    flow
    > kira2 flow dari awal sampe akhir udah mulai tergambar
    > tambahan variabel
    >di dalam program, sebelum masuk simulasi ada bebertapa hal yg bisa dilakukan, dipanggil oleh call0, unit callsim

--- main.pas
+++ main.pas
@@ -1,6 +1,55 @@
 program main;
-
+uses
+	typeuniverse;
+	CallSim;
 var
-
+	energi, hari, nMakan, nIstirahat, nTidur : integer;
+	komando1 : string;
+	eoDay,eoProg : Boolean;
+	invMentah : tabInventoriM;
+	invOlahan : tabInventoriO;
+	
 begin
+	//read file ke array here
+	//read file ke energi dan hari here
+	
+	
+	repeat
+	//dalam suatu simulasi
+	
+		//Call0 here
+		
+		while hari<10 and not(eoprog) do
+		//dalam suatu hari
+		begin
+			eoDay:= false;
+			nIstirahat :=6;
+			nMakan :=3;
+			bTidur := false;
+			
+			repeat 
+				if energi=0 then
+				begin
+					writeln('Energi habis, anda hanya dapat tidur');
+					writeln('> Tidur');
+					tidur(bTidur, energi, eoDay, invMentah, invOlahan);
+				end	
+				else //energi <>0
+				begin
+					Call1(); //main action
+				end;
+			until eoDay or eoProg;
+			
+			if not(eoProg) then
+			begin
+				hari:= hari+1; 
+			end;
+			
+		end;
+		
+	until eoprog = True;
+	
+  
+	//terminasi
+
 end.
