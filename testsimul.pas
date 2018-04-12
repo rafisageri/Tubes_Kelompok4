3e9506c01f16537678ceb2ecab0bf5ca481d3e35
hafizhmh <38131950+hafizhmh@users.noreply.github.com>
2018-04-12 23:57:32 +0700

    Add files via upload
    buat test simul.pas

---
+++ testsimul.pas
@@ -0,0 +1,22 @@
+program test;
+uses
+	simul;
+
+var
+	a : filesimul;
+	name : string;
+	ayyy : listSimulasi;
+	stop : boolean;
+	x : char;
+begin
+	
+	stop:=false;
+	repeat
+		writeln('stop? y/n');
+		readln(x);
+		if x = 'y' then 
+			stop:=true
+		else
+			fSIm2var(a, name, ayyy);
+	until stop;
+end.
