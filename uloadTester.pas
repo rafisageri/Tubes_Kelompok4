f4885235093a82adabff6276e410c203b105c053
rafisageri <38106643+rafisageri@users.noreply.github.com>
2018-04-12 22:10:00 +0700

    Add files via upload

---
+++ uloadTester.pas
@@ -0,0 +1,27 @@
+program uloadTester;
+
+uses uload, typeuniverse, sysutils;
+
+var
+	bahanmentah : tabmentah;
+	bahanolahan : tabolahan;
+	i : integer;
+
+begin
+	LoadBahanMentah(bahanmentah);
+	for i:=1 to 3 do
+	begin
+		writeln (bahanmentah.tab[i].nama);
+		writeln (bahanmentah.tab[i].harga);
+		writeln (bahanmentah.tab[i].kadaluarsa);
+	end;
+	
+	LoadBahanOlahan (bahanolahan);
+	for i:=1 to 2 do
+	begin
+		writeln (bahanolahan.tab[i].nama);
+		writeln (bahanolahan.tab[i].hargaJual);
+		writeln (bahanolahan.tab[i].komposisi[1]);
+		writeln (bahanolahan.tab[i].komposisi[2]);
+	end;
+end.
