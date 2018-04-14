f2760153f90af5b634159ed09168438e509439a2
rafisageri <38106643+rafisageri@users.noreply.github.com>
2018-04-14 17:08:06 +0700

    Tolong dicek
    sortingTester buat ngecek prosedur urutString. Bugnya command prompt nge-hang kalau inputnya perlu disorting. Padahal gua udah cek harusnya gak ada infinite loop di algoritmanya ._.

---
+++ sortingTester.pas
@@ -0,0 +1,51 @@
+program cobalagi;
+
+uses typeuniverse, sysutils;
+
+procedure urutString (var s1, s2 : string); 
+	{I.S string s1 dan s2 terdefinisi dan tidak kosong, bisa sudah terurut atau belum terurut. Contoh : s1='bulan', s2='anak'
+	 F.S s1 dan s2 terurut berdasarkan abjad. Contoh : s1 = 'anak', s2 = 'bulan'}
+	var {KAMUS LOKAL urutString}
+		temp1, temp2, temp : string;
+		i : integer;
+		stop : boolean;
+	{ALGORITMA urutString}
+	begin
+		temp1 := UpperCase (s1);
+		temp2 := UpperCase (s2);
+		i := 1;
+		stop := False;
+		repeat {Skema standar iterate-stop}
+			if (ord(temp1[i]) < ord(temp2[i])) then {Ord () mengembalikan integer yang ekivalen dengan ordinal dari suatu char. Ordnya lebih kecil berarti char tersebut ada di urutan lebih dulu di alfabet}
+			begin
+				stop := True;
+			end else if (ord(temp1[i]) = ord(temp2[i])) then
+			begin
+				i := i + 1;
+			end else {(ord(temp1[i]) > ord(temp2[i]) }
+			begin 
+				temp := s1; {Tukar s1 dengan s2}
+				s1 := s2; 
+				s2 := temp;
+			end;
+		until ((stop) or (i>=length(s1)) or (i>=length(s2)));
+		if (length(s1) > length(s2)) then {Misalkan stringnya halo sama halobandung. Asumsinya halo mau ditulis duluan daripada halobandung}
+		begin
+			temp := s1; {Tukar s1 dengan s2}
+			s1 := s2; 
+			s2 := temp;
+		end;
+	end;
+
+var
+	s1,s2 : string;
+	
+begin
+	readln (s1);
+	writeln ('1');
+	readln (s2);
+	writeln('2');
+	urutString (s1,s2);
+	writeln ('3');
+	writeln (s1, s2);
+end.
