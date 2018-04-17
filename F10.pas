b61280f934716842da71202fbb2a44ad61cac84c
isfanr <38127568+isfanr@users.noreply.github.com>
2018-04-17 23:25:37 +0700

    Add files via upload

--- F10.pas
+++ F10.pas
@@ -1,22 +1,24 @@
-unit istirahat;
+unit F10;
 
 interface
 uses typeuniverse;
-procedure istirahat (var daftarsimulasi: tabSimulasi;  N: integer; var n-istirahat: integer = 0);
+procedure istirahat (var daftarsimulasi: listSimulasi;  N: integer; var nistirahat: integer);
 
 implementation
-procedure istirahat (var daftarsimulasi: tabSimulasi; N: integer; var n-istirahat: integer = 0);
+procedure istirahat (var daftarsimulasi: listSimulasi; N: integer; var nistirahat: integer);
 
 begin
-		if (daftarsimulasi.tab[N].energi => 10) or (n-istirahat >= 6) then
+		nistirahat := 0;
+		if (daftarsimulasi.list[N].energi >= 10) or (nistirahat >= 6) then
 	begin
 		writeln('Anda tidak dapat beristirahat lagi, telah mencapai batas maksimum!');
 	end else
 		begin
-			daftarsimulasi.tab[N].energi := daftarsimulasi.tab[N].energi + 1;
-			if (daftarsimulasi.tab[N].energi > 10) then
-				daftarsimulasi.tab[N].energi := 10
+			daftarsimulasi.list[N].energi := daftarsimulasi.list[N].energi + 1;
+			if (daftarsimulasi.list[N].energi > 10) then
+				daftarsimulasi.list[N].energi := 10;
 
-			n-istirahat := n-istirahat + 1;
+			nistirahat := nistirahat + 1;
 		end;
 end;
+end.
