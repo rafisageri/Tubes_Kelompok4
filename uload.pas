d1ed80f15d2e09e6b06abcff70a9a5b581430274
ilhammuliawan <38129884+ilhammuliawan@users.noreply.github.com>
2018-04-18 00:31:14 +0700

    Add files via upload

--- uload.pas
+++ uload.pas
@@ -7,6 +7,8 @@ interface
 	procedure LoadBahanOlahan (var bahanOlahan : tabolahan); {Membaca file eksternal yang bernama 'bahanolahan.txt' terus dimasukkin ke tipe bentukan olahan}
 	procedure LoadInventoriBahanMentah (tabmentah : tabInventoriM); {Membaca file eksternal yang bernama 'InventoriBahanMentah.txt' terus dimasukkin ke tipe bentukan mentah}
 	procedure LoadInventoriBahanOlahan (tabolahan : tabInventoriO); {Membaca file eksternal yang bernama 'Inventoribahanolahan.txt' terus dimasukkin ke tipe bentukan olahan}
+	procedure LoadResep (var bahanResep : tabResep); {Membaca file eksternal yang bernama 'resep.txt' terus dimasukkin ke tipe bentukan resep}
+
 	
 implementation
 	
@@ -160,7 +162,7 @@ procedure LoadInventoriBahanMentah (tabmentah : tabInventoriM); {Membaca file ek
 		close (inf);
 	end;
 
-	procedure LoadInventoriBahanOlahan (tabolahan : tabInventoriO); {Membaca file eksternal yang bernama 'Inventoribahanolahan.txt' terus dimasukkin ke tipe bentukan olahan}
+procedure LoadInventoriBahanOlahan (tabolahan : tabInventoriO); {Membaca file eksternal yang bernama 'Inventoribahanolahan.txt' terus dimasukkin ke tipe bentukan olahan}
 
 	var {KAMUS LOKAL LoadInventoriBahanOlahan}
 		guardMark 	: integer; {Menandakan posisi karakter guard dalam string}
@@ -201,6 +203,66 @@ procedure LoadInventoriBahanMentah (tabmentah : tabInventoriM); {Membaca file ek
 			temp := copy (temp,guardMark+2, length(temp));
 			tabolahan.tab[j].jumlah := StrToInt (copy (temp, 1, length(temp)));
 		end;
+		close (inf);
+	end;
+
+procedure LoadResep (var bahanResep : tabResep); {Membaca file eksternal yang bernama 'resep.txt' terus dimasukkin ke tipe bentukan resep}
+
+	var {KAMUS LOKAL LoadResep}
+		guardMark 		: integer; {Menandakan posisi karakter guard dalam string}
+		i	 			: integer; {counter traversal}
+		j				: integer; {Menandakan jumlah bahan mentah dalam file eksternal}
+		temp			: string;  {Menyimpan data pada file eksternal secara sementara, bersifat serbaguna}
+		inf				: textfile;
+		lTemp			: integer;	{Menandakan panjang temp}
+	begin {ALGORITMA UTAMA LoadResep}
+		assign (inf, 'resep.txt');
+		reset (inf);
+		j := 0;
+		while (not(EOF(inf))) do
+		begin
+			j := j+1;
+			
+			{Membaca nama resep yang bertipe string}
+			readln(inf, temp);
+			write('ini temp awal ', temp);
+			lTemp := length(temp);
+			guardMark := pos ('|', temp);
+			bahanResep.tab[j].nama := copy(temp, 1, guardMark-2);
+					
+			{Membaca harga jual resep yang bertipe integer}
+			temp := copy (temp,guardMark+2, lTemp);
+			writeln('ini temp pas baca harga jual ', temp);
+			lTemp := length(temp);
+			guardMark := pos ('|', temp);
+			bahanResep.tab[j].harga := StrToInt( copy(temp, 1, guardMark-2) );
+			
+			writeln ('ini tempHargaJual ', bahanResep.tab[j].harga);
+		
+			{Membaca jumlah komposisi bahan mentah dengan asumsi nilai pada file eksternal bisa salah.
+			Sehingga jumlah komposisi bahan mentah ditentukan dengan menghitung nama-nama bahan mentah penyusun yang ada pada file}
+		 
+			temp := copy (temp,guardMark+2, lTemp);			//format temp saat ini nama | nama | ... | nama
+			writeln('temp saat ini: ', temp);				//akan dijadikan nama | nama | ... | nama |
+			temp:= temp + ' |';								//format temp saat ini nama | nama | ... | nama |
+		
+			//asumsi bahanResep.tab[j].N inkonklusif
+			lTemp := length(temp);
+			i:=0;
+		
+			while lTemp <>0 do //temp tidak kosong
+			begin
+				i:=i+1;
+				guardMark := pos ('|', temp);
+				bahanResep.tab[j].komposisi[i].nama := copy(temp, 1, guardMark-2);
+				writeln('nama terekam :', bahanResep.tab[j].komposisi[i].nama);
+				temp := copy (temp,guardMark+2, lTemp); {Jika guardMark+2 lebih dari ltemp, maka copy () akan mengembalikan string kosong}
+				lTemp := length(temp);
+			end;
+			bahanResep.tab[j].N := i;
+			//end of process
+		end;
+		bahanResep.neff := j;
 		close (inf);
 	end;
 end.
