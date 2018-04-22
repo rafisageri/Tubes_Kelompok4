85d22d3e8d8e9650870b1bba3cd4064070961e55
christianjustin <38127582+christianjustin@users.noreply.github.com>
2018-04-22 11:26:46 +0700

    Update F11.pas

--- F11.pas
+++ F11.pas
@@ -2,16 +2,31 @@ unit F11;
 
 interface
 uses typeuniverse;
-procedure tidur(var simulasi: tabSimulasi; N: integer; aktivitas: boolean = false);
+procedure tidur(var Sim: tabSimulasi; N: integer; aktivitas: boolean = false);
 
 implementation
-procedure tidur( var simulasi: tabSimulasi; var B-tidur: boolean; N: integer; aktivitas: boolean = false);
+procedure tidur( var Sim: tabSimulasi; aktivitas: boolean; N: integer; var eoDay: boolean);
 	begin
-		simulasi.tabSimulasi[N].energi := 10;
-		writeln('Anda telah tidur dan tubuh Anda telah kembali fit, yeay!');
-		simulasi.tabSimulasi[N].awalsim.hari := simulasi.tabSimulasi[N].awalsim.hari + 1;
-		writeln('Hari ke-', simulasi.tabSimulasi[N].awalsim.hari);
-//B-tidur blm diimplementasi
+	if (eoDay) then
+		begin
+		Sim.tabSimulasi[N].energi := 10;
+		Sim.tabSimulasi[N].awalsim.hari := Sim.tabSimulasi[N].awalsim.ri + 1;
+		writeln('Anda telah tidur dan Energi Anda telah dipulihkan!');
+		writeln('Hari ke-', Sim.tabSimulasi[N].awalsim.hari);
+		eoDay := false;
+		aktivitas := false;
+		end else if (aktivitas) then 
+			begin
+			Sim.tabSimulasi[N].energi := 10;
+			Sim.tabSimulasi[N].awalsim.hari := Sim.tabSimulasi[N].awalsim.hari + 1;
+			writeln('Anda telah tidur dan Energi Anda telah dipulihkan!');
+			writeln('Hari ke-', Sim.tabSimulasi[N].awalsim.hari);
+			eoDay := false;
+			aktivitas := false;
+			end else writeln('Anda tidak dapat tidur karena Anda belum melakukan apapun');
+		
+//variabel 'boleh-tidur' blm terdefinisi
+//
 
 function cariKadaluarsa(Barang: string; daftarMentah: tabMentah) : integer
 var
@@ -26,24 +41,27 @@ begin
 	end;
 end;
 
-procedure buang(var invenMentah: tabInventoriM; var invenOlahan: tabInventoriO; simulasi: tabSimulasi; N: integer; daftarMentah: tabMentah);
+procedure buang(var invenMentah: tabInventoriM; var invenOlahan: tabInventoriO; Sim: tabSimulasi; N: integer; daftarMentah: tabMentah);
 var
-	Nkad: integer;
+	Nkad: integer; //banyak barang yg sudah kadaluarsa
 	i: integer;
-	Temp: inventoriM;
+	TempM: inventoriM; //variabel sementara untuk bahan mentah
+	TempO: inventoriO; //variabel sementara untuk bahan olahan
 begin
 	Nkad:= 0;
 	for i:=1 to invenMentah.Neff do
 	begin	
 		////////////////////////////////////////////////////////////////////////////
 		//CEK INI
-		if (simulasi.simulasi[N].awalsim.tanggal.hari >= InvenMentah.InventoriM[i].tglbeli.hari + cariKadaluarsa(invenMentah.inventoriM[i].nama, daftarMentah)) then
+		
+		//Mengolah bahan mentah
+		if (Sim.simulasi[N].awalsim.tanggal.hari >= InvenMentah.InventoriM[i].tglbeli.hari + cariKadaluarsa(invenMentah.inventoriM[i].nama, daftarMentah)) then
 		begin
-			Temp.nama := invenMentah.inventoriM[i].nama;
-			Temp.tglbeli.hari := invenMentah.inventoriM[i].tglbeli.hari;
-			Temp.tglbeli.bulan := invenMentah.inventoriM[i].tglbeli.bulan;
-			Temp.tglbeli.tahun := invenMentah.inventoriM[i].tglbeli.tahun;
-			Temp.jumlah := invenMentah.inventoriM[i].jumlah;
+			TempM.nama := invenMentah.inventoriM[i].nama;
+			TempM.tglbeli.hari := invenMentah.inventoriM[i].tglbeli.hari;
+			TempM.tglbeli.bulan := invenMentah.inventoriM[i].tglbeli.bulan;
+			TempM.tglbeli.tahun := invenMentah.inventoriM[i].tglbeli.tahun;
+			TempM.jumlah := invenMentah.inventoriM[i].jumlah;
 
 			invenMentah.inventoriM[i].nama := invenMentah.inventoriM[invenMentah.Neff-Nkad].nama;
 			invenMentah.inventoriM[i].tglbeli.hari := invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.hari;
@@ -51,11 +69,11 @@ begin
 			invenMentah.inventoriM[i].tglbeli.tahun := invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.tahun;
 			invenMentah.inventoriM[i].jumlah := invenMentah.inventoriM[invenMentah.Neff-Nkad].jumlah;
 
-			invenMentah.inventoriM[invenMentah.Neff-Nkad].nama := Temp.nama;
-			invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.hari := Temp.tglbeli.hari;
-			invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.bulan := Temp.tglbeli.bulan;
-			invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.tahun := Temp.tglbeli.tahun;
-			invenMentah.inventoriM[invenMentah.Neff-Nkad].jumlah := Temp.jumlah;
+			invenMentah.inventoriM[invenMentah.Neff-Nkad].nama := TempM.nama;
+			invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.hari := TempM.tglbeli.hari;
+			invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.bulan := TempM.tglbeli.bulan;
+			invenMentah.inventoriM[invenMentah.Neff-Nkad].tglbeli.tahun := TempM.tglbeli.tahun;
+			invenMentah.inventoriM[invenMentah.Neff-Nkad].jumlah := TempM.jumlah;
 
 			Nkad := Nkad + 1;			
 		end;
@@ -63,16 +81,18 @@ begin
 
 	invenMentah.Neff := invenMentah.Neff - Nkad;
 
+	//menginisialisasi kembali jumlah kadaluarsa
+	//mengolah bahan olahan
 	Nkad:= 0;
 	for i:=1 to invenOlahan.Neff do
 	begin
-		if (simulasi.simulasi[N].awalsim.tanggal.hari >= InvenOlahan.InventoriO[i].tglbeli.hari + 3) then
+		if (Sim.simulasi[N].awalsim.tanggal.hari >= InvenOlahan.InventoriO[i].tglbeli.hari + 3) then
 		begin
-			Temp.nama := invenOlahan.inventoriO[i].nama;
-			Temp.tglbeli.hari := invenOlahan.inventoriO[i].tglbeli.hari;
-			Temp.tglbeli.bulan := invenOlahan.inventoriO[i].tglbeli.bulan;
-			Temp.tglbeli.tahun := invenOlahan.inventoriO[i].tglbeli.tahun;
-			Temp.jumlah := invenOlahan.inventoriO[i].jumlah;
+			TempO.nama := invenOlahan.inventoriO[i].nama;
+			TempO.tglbeli.hari := invenOlahan.inventoriO[i].tglbeli.hari;
+			TempO.tglbeli.bulan := invenOlahan.inventoriO[i].tglbeli.bulan;
+			TempO.tglbeli.tahun := invenOlahan.inventoriO[i].tglbeli.tahun;
+			TempO.jumlah := invenOlahan.inventoriO[i].jumlah;
 
 			invenOlahan.inventoriO[i].nama := invenOlahan.inventoriO[invenOlahan.Neff-Nkad].nama;
 			invenOlahan.inventoriO[i].tglbeli.hari := invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.hari;
@@ -80,11 +100,11 @@ begin
 			invenOlahan.inventoriO[i].tglbeli.tahun := invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.tahun;
 			invenOlahan.inventoriO[i].jumlah := invenOlahan.inventoriO[invenOlahan.Neff-Nkad].jumlah;
 
-			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].nama := Temp.nama;
-			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.hari := Temp.tglbeli.hari;
-			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.bulan := Temp.tglbeli.bulan;
-			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.tahun := Temp.tglbeli.tahun;
-			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].jumlah := Temp.jumlah;
+			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].nama := TempO.nama;
+			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.hari := TempO.tglbeli.hari;
+			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.bulan := TempO.tglbeli.bulan;
+			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].tglbeli.tahun := TempO.tglbeli.tahun;
+			invenOlahan.inventoriO[invenOlahan.Neff-Nkad].jumlah := TempO.jumlah;
 
 			Nkad := Nkad + 1;			
 		end;
